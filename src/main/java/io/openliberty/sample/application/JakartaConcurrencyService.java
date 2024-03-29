fc9dc05f6ec517cdb4b3738d3d3d92e277d1ce65
Gautham Krishnan <140151984+gauthamkrishnanibm@users.noreply.github.com>
2024-03-30 01:57:27 +0530

    Initial commit

---
+++ src/main/java/io/openliberty/sample/application/JakartaConcurrencyService.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+* Copyright (c) 2024 IBM Corporation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v2.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v20.html
+*
+* Contributors:
+*     IBM Corporation - initial API and implementation
+*******************************************************************************/
+package io.openliberty.sample.application;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import jakarta.enterprise.concurrent.ContextService;
+import jakarta.enterprise.concurrent.ManagedExecutorService;
+import jakarta.enterprise.concurrent.ManagedThreadFactory;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api")
+@ApplicationScoped
+public class JakartaConcurrencyService {
+
+    @Inject
+    ContextService contextService;
+
+    @Inject
+    ManagedExecutorService managedExecutorService;
+
+    @Inject
+    ManagedThreadFactory managedThreadFactory;
+
+
+    @GET
+    @Path("managedExecuters")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getmanagedExecuters() throws InterruptedException, ExecutionException {
+        Future future1 = managedExecutorService.submit(() -> {
+            System.out.println("Job 1 running ...");
+            // This takes some while
+            System.out.println("Job 1 finished ...");
+        });
+        Future future2 = managedExecutorService.submit(() -> {
+            System.out.println("Job 2 running ...");
+            // This takes some while
+            System.out.println("Job 2 finished ...");
+        });
+        System.out.println("Jobs completed");
+        return "Managed Executer jobs Submitted!";
+    }
+
+    @GET
+    @Path("managedThreads")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getManagedThreads() throws InterruptedException, ExecutionException {
+        // Create and start a managed thread
+        Thread thread = managedThreadFactory.newThread(() -> {
+            System.out.println("Managed thread is executing");
+        });
+        thread.start();
+        return "Managed thread created!";
+    }
+
+    @GET
+    @Path("contextPropagation")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getContextPropagation() throws InterruptedException, ExecutionException {
+
+        managedExecutorService.execute(() -> {
+            try {
+                System.out.println(new InitialContext().lookup("java:comp/env/replySuccess"));
+            } catch (NamingException namingException) {
+                namingException.printStackTrace();
+            }
+        });
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        
+        //Executer without context
+        executor.submit(() -> {
+            try {
+                System.out.println(new InitialContext().lookup("java:comp/env/replySuccess"));
+            } catch (NamingException namingException) {
+                System.err.println("NamingException on ExecuterService as no context available.");
+            }
+        });
+
+        executor.submit(contextService.contextualRunnable(()-> {
+            try {
+              System.out.println(new InitialContext().lookup("java:comp/env/replySuccess"));             
+            } catch (NamingException namingException) {
+                namingException.printStackTrace();
+            }
+          }));
+
+        return "Application context propogated with java.util.concurrent.ExecutorService using jakarta.enterprise.concurrent.ContextService";
+    }
+
+    
+}
